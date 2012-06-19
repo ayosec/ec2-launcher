7cf5d3283c55fe74bd82db3d4428b7c5c8fabb97
Ayose <ayosec@gmail.com>
2012-06-19 13:27:21 +0100

    Nicer interface

--- app/core/Config.scala
+++ app/core/Config.scala
@@ -4,4 +4,6 @@ object Config {
   final val awsAccessKey = System.getenv("AWS_ACCESS_KEY")
   final val awsSecretKey = System.getenv("AWS_SECRET_KEY")
   final val eccInstance = System.getenv("EC_INSTANCE")
+  final val backgroundURL = System.getenv("BG_URL")
+  final val fontName = System.getenv("FONT_NAME")
 }
